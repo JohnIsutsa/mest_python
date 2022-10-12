c4656018a03a31fc43b17c3ac1f53afd2e9960c3
John Isutsah <johnisutsa@gmail.com>
2022-10-12 08:57:12 +0000

    add two numbers

---
+++ intro.py
@@ -0,0 +1,11 @@
+# print("Hello world. My names is John Isutsah.")
+
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+
+def addNumber(first, second):
+    sum = first + second
+    return sum
+
+print(addNumber(firstNumber, secondNumber))
+
